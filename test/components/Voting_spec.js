53f994776680a26c7059297c58acbed0503f8db2
jeremiah <jeremiahrichter@gmail.com>
2016-09-19 18:57:02 -0500

    check for pure rendering

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} from "react-addons-test-utils";
 import Voting from "../../src/components/Voting";
 import {expect} from "chai";
+import {List} from 'immutable';
 
 describe('Voting', () => {
 
@@ -65,4 +66,25 @@ describe('Voting', () => {
         expect(winner).to.be.ok;
         expect(winner.textContent).to.contain('Trainspotting');
     });
+
+    it('does update DOM when prop changes', () => {
+        const pair = List.of('Trainspotting', '28 Days Later');
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+
+        const newPair = pair.set(0, 'Sunshine');
+        component = ReactDOM.render(
+            <Voting pair={newPair} />,
+            container
+        );
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Sunshine');
+    });
+
 });
