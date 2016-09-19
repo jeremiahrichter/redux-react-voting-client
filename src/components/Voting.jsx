a1260e75be387c9f6241f1097ff401da0715e5b4
jeremiah <jeremiahrichter@gmail.com>
2016-09-19 18:22:30 -0500

    add in disabling and winning

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,12 +4,26 @@ export default React.createClass({
     getPair: function () {
         return this.props.pair || [];
     },
+    isDisabled: () => {
+        return !!this.props.hasVoted;
+    },
+    hasVotedFor: (entry) => {
+        return this.props.hasVoted === entry;
+    },
     render: function () {
         return <div className="voting">
-            {this.getPair().map(entry =>
+            {this.props.winner?
+                <div ref="winner">The winner is {this.props.winner}!</div>:
+                this.getPair().map(entry =>
                 <button key={entry}
-                        onClick={() => this.props.vote(entry)}>
+                        onClick={() => this.props.vote(entry)}
+                        disabled={this.props.isDisabled()}>
                     <h1>{entry}</h1>
+                    {
+                        this.hasVotedFor(entry) ?
+                            <div className="label">Voted</div> :
+                            null
+                    }
                 </button>
             )}
         </div>;
