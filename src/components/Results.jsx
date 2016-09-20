9bf363c4b1320ea10e27f84a561e33f5c9c27e76
jeremiah <jeremiahrichter@gmail.com>
2016-09-19 20:24:51 -0500

    add vote display

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,6 +3,13 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
     mixins: [PureRenderMixin],
+    getVotes: function () {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        } else {
+            return 0;
+        }
+    },
     getPair: function () {
         return this.props.pair || [];
     },
@@ -11,6 +18,9 @@ export default React.createClass({
             {this.getPair().map(entry =>
                 <div key={entry} className="entry">
                     <h1>{entry}</h1>
+                    <div className="voteCount">
+                        {this.getVotes(entry)}
+                    </div>
                 </div>
             )}
         </div>;
