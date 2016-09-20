de522e8d36663f5f66fc45b148474564636134b8
jeremiah <jeremiahrichter@gmail.com>
2016-09-19 20:18:45 -0500

    add basic Results

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import Voting from "./components/Voting";
 import {Route, Router, hashHistory} from "react-router";
 import App from "./components/App"
+import Results from "./components/Results";
 
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
-    <Route path="/" component={Voting} />
+    <Route path="/results" component={Results}/>
+    <Route path="/" component={Voting}/>
 </Route>;
 
 ReactDOM.render(
