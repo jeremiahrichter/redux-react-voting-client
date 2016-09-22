01685ca0d595d8497290385d5daba18f27085596
jeremiah <jeremiahrichter@gmail.com>
2016-09-21 20:33:37 -0500

    connect state to web app through websocket

--- src/index.jsx
+++ src/index.jsx
@@ -7,21 +7,16 @@ import {ResultsContainer} from "./components/Results";
 import reducer from './reducer';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 
 const store = createStore(reducer);
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: [
-                'Trainspotting',
-                '28 Days Later'
-            ],
-            tally: {
-                'Sunshine': 2
-            }
-        }
-    }
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => {
+    store.dispatch({
+        type: 'SET_STATE',
+        state: state
+    });
 });
 
 const routes = <Route component={App}>
