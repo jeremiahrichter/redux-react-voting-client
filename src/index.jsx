21f792cafe3a54e0bb5197722b56540a5cc5c5fd
jeremiah <jeremiahrichter@gmail.com>
2016-09-23 18:40:55 -0500

    connect vote action

--- src/index.jsx
+++ src/index.jsx
@@ -11,15 +11,16 @@ import io from 'socket.io-client';
 import {setState} from './action_creators';
 import remoteActionMiddleware from './remote_action_middleware';
 
-const createStoreWithMiddleware = applyMiddleware(
-    remoteActionMiddleware
-)(createStore);
-const store = createStoreWithMiddleware(reducer);
-
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => {
     store.dispatch(setState(state));
 });
+
+const createStoreWithMiddleware = applyMiddleware(
+    remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer}/>
