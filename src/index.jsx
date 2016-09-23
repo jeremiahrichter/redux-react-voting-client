9cb81f74f6fc85fc25536540d39b1aefd4a473c4
jeremiah <jeremiahrichter@gmail.com>
2016-09-23 18:31:51 -0500

    add middleware to redux

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,16 @@ import {Route, Router, hashHistory} from "react-router";
 import App from "./components/App"
 import {ResultsContainer} from "./components/Results";
 import reducer from './reducer';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
-import {setState} from './action_creators'
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 
-const store = createStore(reducer);
+const createStoreWithMiddleware = applyMiddleware(
+    remoteActionMiddleware
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => {
